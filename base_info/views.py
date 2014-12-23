2b2881390d8bdf29d87c6c419eaea6567e6af9b4
yfliubing <yfliubing@jd.com>
2014-12-23 13:43:02 +0800

    modify define_* process, add update process

--- base_info/views.py
+++ base_info/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import logging
 from django.db import transaction
 
@@ -37,22 +38,44 @@ def define_goods(request):
                         date={'error': 'Attribute[\'updater\'] can not be none.'})
 
     try:
-        goods = Goods(
-            goodsCode=message.get('goods_code'),
-            goodsType=message.get('goods_type'),
-            goodsName=message.get('goods_name'),
-            goodsPrice=message.get('goods_price'),
-            goodsUnit=message.get('goods_unit'),
-            barcode=message.get('barcode'),
-            isbn=message.get('isbn'),
-            productDate=message.get('product_date'),
-            yn=message.get('yn'),
-            createTime=message.get('create_time'),
-            updateTime=message.get('update_time'),
-            creator=message.get('creator'),
-            updater=message.get('updater'),
-            goodsDesc=message.get('goods_desc'),
+        queryCount = Goods.objects.filter(goodsCode=message.get('goods_code')).count()
+        if queryCount == 0:
+            """
+                if query result is none
+                    this is a new record. Create it to db.
+                else
+                    Update it exclude goods_code.
+            """
+            nowTime = datetime.now()
+            goods = Goods(
+                goodsCode=message.get('goods_code'),
+                goodsType=message.get('goods_type'),
+                goodsName=message.get('goods_name'),
+                goodsPrice=message.get('goods_price'),
+                goodsUnit=message.get('goods_unit'),
+                barcode=message.get('barcode'),
+                isbn=message.get('isbn'),
+                productDate=message.get('product_date'),
+                yn=message.get('yn'),
+                createTime=nowTime,
+                updateTime=nowTime,
+                creator=message.get('creator'),
+                updater=message.get('updater'),
+                goodsDesc=message.get('goods_desc'),
             )
+        else:
+            goods = Goods.objects.get(goodsCode=message.get('goods_code'))
+            goods.goodsType = message.get('goods_type'),
+            goods.goodsName = message.get('goods_name'),
+            goods.goodsPrice = message.get('goods_price'),
+            goods.goodsUnit = message.get('goods_unit'),
+            goods.barcode = message.get('barcode'),
+            goods.isbn = message.get('isbn'),
+            goods.productDate = message.get('product_date'),
+            goods.yn = message.get('yn'),
+            goods.updateTime = datetime.now(),
+            goods.updater = message.get('updater'),
+            goods.goodsDesc = message.get('goods_desc'),
         goods.save()
     except Exception as e:
         LOG.error('Goods Information saved error.\n [ERROR]:%s' % str(e))
@@ -81,6 +104,29 @@ def define_product(request):
                         date={'error': 'Attribute[\'details\'] must be a non-empty array.'})
 
     try:
+        querycount = Product.objects.filter(product_code=product_code)
+        if querycount != 0:
+            ProductDetails.objects.filter(product_code=product_code).delete()
+            product = Product.objects.get(product_code=product_code)
+            product.product_name = message.get('product_name'),
+            product.product_code = message.get('product_code'),
+            product.product_desc = message.get('product_desc'),
+            product.updater = message.get('updater'),
+            product.update_time = datetime.now(),
+            product.yn = message.get('yn'),
+        else:
+            nowTime = datetime.now()
+            product = Product(
+                product_name=message.get('product_name'),
+                product_code=message.get('product_code'),
+                product_desc=message.get('product_desc'),
+                creator=message.get('creator'),
+                create_time=nowTime,
+                updater=message.get('updater'),
+                update_time=nowTime,
+                yn=message.get('yn'),
+            )
+        product.save()
         for detail in details:
             if detail.get('goods_code') is None:
                 raise ValueIsNoneException('The goods code can not be none.')
@@ -91,17 +137,6 @@ def define_product(request):
                 is_gift=detail.get('is_gift'),
             )
             product_detail.save()
-        product = Product(
-            product_name=message.get('product_name'),
-            product_code=message.get('product_code'),
-            product_desc=message.get('product_desc'),
-            creator=message.get('creator'),
-            create_time=message.get('create_time'),
-            updater=message.get('updater'),
-            update_time=message.get('update_time'),
-            yn=message.get('yn'),
-        )
-        product.save()
         transaction.commit()
     except Exception as e:
         LOG.error('Product Information saved error.\n [ERROR]:%s' % str(e))
