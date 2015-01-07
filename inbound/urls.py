42bcca23e7253c3343c669b5ba7b986467d8ac0c
yfliubing <yfliubing@jd.com>
2015-01-07 22:20:39 +0800

    fixed bug

--- inbound/urls.py
+++ inbound/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns(
     'inbound.views',
     url(r'^receipt/create/$', 'create_receipt'),
     url(r'^receipt/(.+)/cancel/$', 'cancel_receipt'),
-    url(r'^putin/$', 'put_in'),
+    url(r'^putin/$', 'putin'),
     url(r'^receipts/$', 'query_receipt_list'),
     url(r'^receipt/(.+)/$', 'query_receipt'),
 )
