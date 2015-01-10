663e6907687c72a89e01c9225db324b100cab354
yfliubing <yfliubing@jd.com>
2015-01-10 14:33:37 +0800

    modify paths

--- inner/urls.py
+++ inner/urls.py
@@ -5,8 +5,8 @@ __author__ = 'pluto'
 
 urlpatterns = patterns(
     'inner.views',
-    url(r'^records/(.+)/$', 'query_records'),
-    url(r'^goods/(.+)/$', 'query_goods'),
-    url(r'^products/(.+)/$', 'query_products'),
-    url(r'^picking/(.+)/$', 'picking'),
+    url(r'^(.+)/records/$', 'query_records'),
+    url(r'^(.+)/goods/$', 'query_goods'),
+    url(r'^(.+)/products/$', 'query_products'),
+    url(r'^(.+)/picking/$', 'picking'),
 )
