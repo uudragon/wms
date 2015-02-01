73a351460aca192fae5efefbaea30dca1c0a797d
yfliubing <yfliubing@jd.com>
2015-02-01 21:31:27 +0800

    fixed bugs

--- outbound/urls.py
+++ outbound/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns(
     'outbound.views',
     url(r'^split/$', 'split'),
     url(r'^shipment/save/$', 'save_shipment'),
+    url(r'^shipment/amount_setting/$', 'set_orders_amount'),
     url(r'^shipments/$', 'query_shipments'),
     url(r'^shipment/(.+)/$', 'query_shipment'),
     url(r'^shipment/(.+)/check/$', 'check'),
