5db1838a60ae1c68afae1eeb42b16987d81d8b06
yfliubing <yfliubing@jd.com>
2015-02-27 20:46:54 +0800

    fixed bugs

--- outbound/urls.py
+++ outbound/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns(
     url(r'^picking_orders/create/$', 'assemble_picking_orders'),
     url(r'^picking_orders/picking_completed/(.+)/$', 'picking_completed'),
     url(r'^picking_orders/picking/(.+)/$', 'picking'),
+    url(r'^picking_orders/$', 'query_picking_orders_list'),
     url(r'^shipment/sent/$', 'sent'),
     url(r'^shipments/orders(.+)/$', 'query_by_ordersno'),
     url(r'^picking_orders/(.+)/$', 'query_single_picking_orders'),
