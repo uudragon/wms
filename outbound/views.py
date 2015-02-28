b9e9019794a3ba4f703759a2b56fb9532130ff62
yfliubing <yfliubing@jd.com>
2015-02-28 22:23:08 +0800

    fixed bugs

--- outbound/views.py
+++ outbound/views.py
@@ -760,7 +760,7 @@ def assemble_picking_orders(request):
         shipments = Shipment.objects.select_for_update().filter(shipment_no__in=message.get('shipment_nos'))
         effective_nos = []
         for shipment in shipments:
-            if shipment.status < 2:
+            if shipment.status == 1:
                 effective_nos.append(shipment.shipment_no)
         else:
             message.pop('shipment_nos')
@@ -846,7 +846,7 @@ def query_picking_orders_list(request):
     now_time = datetime.now()
     try:
         query_month = int(message.get('month')) if message.get('month') is not None else now_time.month
-        query_list = PickingOrders.objects.filter(create_time__month=query_month, create_time__year=now_time.year, status=0).order_by('create_time')
+        query_list = PickingOrders.objects.filter(create_time__month=query_month, create_time__year=now_time.year, status=1).order_by('create_time')
         paginator = Paginator(query_list, pageSize, orphans=0, allow_empty_first_page=True)
         total_page_count = paginator.num_pages
         if pageNo > total_page_count:
