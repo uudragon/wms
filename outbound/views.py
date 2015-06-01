6817bee553bcd607c2809bc815d1c5280f0d9d37
yfliubing <yfliubing@jd.com>
2015-06-01 11:23:28 +0800

    fixed bugs

--- outbound/views.py
+++ outbound/views.py
@@ -1130,7 +1130,7 @@ def request_express(shipment, shipment_details):
                 success_nodes = root.getElementsByTagName('success')
                 LOG.debug('---------> %s ' % len(success_nodes))
                 success_node = success_nodes[0]
-                LOG.debug(success_node.nodeValue)
+                LOG.debug(dir(success_node))
                 if bool(success_node.nodeValue):
                     LOG.debug('------->success')
                     if len(root.getElementsByTagName('noticeMessage')) != 0:
