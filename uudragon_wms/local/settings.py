c0c9a391a596f3d5b0fe2d26b2a2e0835c557211
yfliubing <yfliubing@jd.com>
2015-06-04 14:17:40 +0800

    update yto service url

--- uudragon_wms/local/settings.py
+++ uudragon_wms/local/settings.py
@@ -22,15 +22,16 @@ INBOUND_RECEIPT_DETAIL_STATUS_COMPLETED = 2
 STORAGE_RECORD_TYPE_RECEIPT = 1
 STORAGE_RECORD_TYPE_OUTPUT = 5
 
-# CLIENT_ID = 'K100511456'
-# PRIVATE_KEY = 'U45BawXC'
-CLIENT_ID = 'K24000154'
-PRIVATE_KEY = 'weH71Rbq'
+CLIENT_ID = 'K100511456'
+PRIVATE_KEY = 'U45BawXC'
+#CLIENT_ID = 'K24000154'
+#PRIVATE_KEY = 'weH71Rbq'
 LOGISTIC_PROVIDER_ID = 'YTO'
 
 TO_SENDER_REQUEST_HEADERS = {'content-type': 'application/x-www-form-urlencoded',
                         'Accept': 'application/x-www-form-urlencoded'}
 
-SENDER_SERVICE_API = 'http://58.32.246.71:8088/CommonOrderModeBServlet.action'
+#SENDER_SERVICE_API = 'http://58.32.246.71:8088/CommonOrderModeBServlet.action'
+SENDER_SERVICE_API = 'http://service.yto56.net.cn/CommonOrderModeBServlet.action'
 
 DEFAULT_SENDER_NAME = '\xe4\xbc\x98\xe4\xbc\x98\xe9\xbe\x99\xe6\x95\x99\xe8\x82\xb2'
