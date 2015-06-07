9a064862721316ae20bcdb396993d76fbb619ac7
yfliubing <yfliubing@jd.com>
2015-06-07 21:18:15 +0800

    add sync_shipments method

--- outbound/views.py
+++ outbound/views.py
@@ -1508,20 +1508,20 @@ def sync_shipments(request):
 
     conditions = dict()
     if params.get('agent_code') is not None:
-        conditions['agent_code__exact'] = params.get('agent_code')
+        conditions['agent_code__exact'] = params.get('agent_code')[0]
 
     if params.get('begin_date') is None:
         LOG.error('Attribute[\'begin_date\'] can not be empty.')
         return Response(status=status.HTTP_400_BAD_REQUEST,
                         content_type='application/json;charset=utf-8',
                         data={'error': 'Attribute[\'begin_date\'] can not be empty.'})
-    conditions['sent_date__gte'] = datetime.strftime(params.get('begin_date'), '%Y-%m-%d')
+    conditions['sent_date__gte'] = datetime.strftime(params.get('begin_date')[0], '%Y-%m-%d')
     if params.get('end_date') is None:
         LOG.error('Attribute[\'end_date\'] can not be empty.')
         return Response(status=status.HTTP_400_BAD_REQUEST,
                         content_type='application/json;charset=utf-8',
                         data={'error': 'Attribute[\'end_date\'] can not be empty.'})
-    conditions['sent_date__lte'] = datetime.strftime(params.get('end_date'), '%Y-%m-%d')
+    conditions['sent_date__lte'] = datetime.strftime(params.get('end_date')[0], '%Y-%m-%d')
 
     response_body = []
     try:
